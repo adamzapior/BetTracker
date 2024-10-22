22cae8f89293dd5bdd7b2acbfccdd748f1bb2968
Adam Zapiór <adamzapior@icloud.com>
2024-10-22 20:18:20 +0200

    feat: new router key

--- BetTracker/Routers/Extensions/EnvironmentValues+Extensions.swift
+++ BetTracker/Routers/Extensions/EnvironmentValues+Extensions.swift
@@ -1,9 +1,3 @@
-//
-//  EnvironmentValues+Extensions.swift
-//  RoutersDemo
-//
-//  Created by Itay Amzaleg on 10/03/2024.
-//
 
 import SwiftUI
 
@@ -11,15 +5,49 @@ struct CurrentTabKey: EnvironmentKey {
     static var defaultValue: Binding<ContentView.Tab> = .constant(.a)
 }
 
+// MARK: FeedTabRouter keys
+
+struct BetsDetailsRouterKey: EnvironmentKey {
+    static let defaultValue: any BetsDetailsNavigationProtocol = FeedTabRouter()
+}
+
+struct BetslipDetailsRouterKey: EnvironmentKey {
+    static let defaultValue: any BetslipDetailsNavigationProtocol = FeedTabRouter()
+}
+
+// MARK: ProfileTabRouter keys
+
+struct PreferencesRouterKey: EnvironmentKey {
+    static let defaultValue: any PreferencesNavigationProtocol = ProfileTabRouter()
+}
+
+
 extension EnvironmentValues {
     var currentTab: Binding<ContentView.Tab> {
         get { self[CurrentTabKey.self] }
         set { self[CurrentTabKey.self] = newValue }
     }
+
+    var betsDetailsRouter: any BetsDetailsNavigationProtocol {
+        get { self[BetsDetailsRouterKey.self] }
+        set { self[BetsDetailsRouterKey.self] = newValue }
+    }
+
+    var betslipDetailsRouter: any BetslipDetailsNavigationProtocol {
+        get { self[BetslipDetailsRouterKey.self] }
+        set { self[BetslipDetailsRouterKey.self] = newValue }
+    }
     
-//    
+    var preferencesRouter: any PreferencesNavigationProtocol {
+        get { self[PreferencesRouterKey.self] }
+        set { self[PreferencesRouterKey.self] = newValue }
+    }
+
+//
 //    var presentedSheet: Binding<PresentedSheet?> {
 //        get { self[PresentedSheetKey.self] }
 //        set { self[PresentedSheetKey.self] = newValue }
 //    }
 }
+
+
